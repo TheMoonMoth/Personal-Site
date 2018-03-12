aad1a1e8810a40c1ae460389502de127d17c2eda
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-03-11 18:03:35 -0600

    PW collapsing module complete

--- src/Projects/Project1/index.js
+++ src/Projects/Project1/index.js
@@ -1,13 +1,43 @@
 import React from "react"
+import {presets} from "react-motion"
+import Collapse from "react-collapse"
+import {UnmountClosed} from "react-collapse"
+import PwModal from "./Modal"
 import "./style.css"
 
-const Proj1 = (props) => {
-  return (
-    <section className="project-slide">
-      <img id="pw-logo"src="./images/PlanetWagerLogo.png" alt="Planet Wager Logo" />
-      <h3 onClick={console.log(this.props)}>▼</h3>
-    </section>
-  )
+class Proj1 extends React.Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      isOpened: true,
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <section className="project-slide">
+          <img id="pw-logo"src="./images/PlanetWagerLogo.png" alt="Planet Wager Logo" />
+          <h3 onClick={(e)=>{this.setState({ isOpened: !this.state.isOpened })}} >
+            ▼
+          </h3>
+        </section>
+
+        <Collapse isOpened={this.state.isOpened}>
+          <PwModal />
+        </Collapse>
+      </div>
+    )
+  }
+
+
+
+
+
+
+
+  //   )
+  // }
 }
 
 export default Proj1
--- src/Projects/index.js
+++ src/Projects/index.js
@@ -14,22 +14,16 @@ class Projects extends React.Component {
       absInfo: false,
       wereInfo: false
     }
-    this.infoClick = this.infoClick.bind(this)
-  }
-
-  infoClick = (e) => {
-    e.preventDefault()
-    console.log(e.target)
   }
 
   render() {
     return (
       <div id="projects-section">
         <h2>Projects</h2>
-        <Proj1 status={this.state.pwInfo} ic={this.infoClick} />
-        <Proj2 status={this.state.swsInfo} ic={this.infoClick} />
-        <Proj3 status={this.state.absInfo} ic={this.infoClick} />
-        <Proj4 status={this.state.wereInfo} ic={this.infoClick} />
+        <Proj1 status={this.state.pwInfo} />
+        <Proj2 status={this.state.swsInfo} />
+        <Proj3 status={this.state.absInfo} />
+        <Proj4 status={this.state.wereInfo} />
       </div>
     )
   }
