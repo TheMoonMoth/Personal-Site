9265af64ba8140e234653555a5642f7982df028d
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-03-13 11:26:06 -0600

    h3 color change and new contact icons

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -7,16 +7,16 @@ const Contact = props => {
       <h2>CONTACT</h2>
       <nav>
         <a href="https://github.com/TheMoonMoth">
-          <img src="./images/003-github.png" alt="Github link" />
+          <img src="./images/github.png" alt="Github link" />
         </a>
         <a href="https://linkedin.com/in/KyleWeintraub">
-          <img src="./images/002-linkedin.png" alt="LinkedIn link" />
+          <img src="./images/linkedin.png" alt="LinkedIn link" />
         </a>
         <a href="https://talent.galvanize.com/students/2071">
           <img src="./images/gSchool-icon.png" alt="Galvanize talent link" />
         </a>
         <a href="mailto:weintraub.kyle@gmail.com">
-          <img src="./images/001-mail.png" alt="Email link" />
+          <img src="./images/email.png" alt="Email link" />
         </a>
       </nav>
     </footer>
