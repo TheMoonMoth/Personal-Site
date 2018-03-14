2ffef4f71065bbd451ac5e989b2f657d68c1bc6c
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-03-14 13:45:58 -0600

    style lock, content lock, V1 done

--- src/Resume/index.js
+++ src/Resume/index.js
@@ -10,16 +10,16 @@ class Resume extends React.Component {
         </a>
         <h2>RESUME</h2>
         <aside>
-          <img src="./images/resume.jpg" alt="resume" />
+          <a href="./images/resume.jpg" target="_blank" ><img src="./images/resume.jpg" alt="resume" /></a>
           <div>
             <ul>
               <li>
                 <h3>THE GOOD PARTS</h3>
               </li>
-              <li>Galvanize Graduate</li>
-              <li>Multidisciplined</li>
-              <li>Experience With Instructional Design</li>
-              <li>Project and Team Focused</li>
+              <li>- Galvanize Graduate</li>
+              <li>- Multidisciplined</li>
+              <li>- Experience With Instructional Design</li>
+              <li>- Project and Team Focused</li>
             </ul>
             <a href="https://drive.google.com/uc?export=download&id=1tKx-S8wpbisEUi1lnn67u9pojHMF_SAP">
             DOWNLOAD NOW
