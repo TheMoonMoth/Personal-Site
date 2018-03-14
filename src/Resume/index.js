912da6bd3d8a15cebbccd3f9d8121699dae34725
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-03-14 12:07:48 -0600

    reflexing resume box

--- src/Resume/index.js
+++ src/Resume/index.js
@@ -1,14 +1,31 @@
 import React from "react"
 import "./style.css"
 
-class Resume extends React.Component{
-  render(){
+class Resume extends React.Component {
+  render() {
     return (
       <div id="resume-box">
+        <a name="resume" id="resume-link">
+          {" "}
+        </a>
         <h2>RESUME</h2>
-        <a name="resume" id="resume-link"> </a>
-        <img src="./images/resume.jpg" alt="resume" />
-        <a href="https://drive.google.com/uc?export=download&id=1tKx-S8wpbisEUi1lnn67u9pojHMF_SAP">DOWNLOAD NOW</a>
+        <aside>
+          <img src="./images/resume.jpg" alt="resume" />
+          <div>
+            <ul>
+              <li>
+                <h3>THE GOOD PARTS</h3>
+              </li>
+              <li>Galvanize Graduate</li>
+              <li>Multidisciplined</li>
+              <li>Experience With Instructional Design</li>
+              <li>Project and Team Focused</li>
+            </ul>
+            <a href="https://drive.google.com/uc?export=download&id=1tKx-S8wpbisEUi1lnn67u9pojHMF_SAP">
+            DOWNLOAD NOW
+            </a>
+            </div>
+        </aside>
       </div>
     )
   }
