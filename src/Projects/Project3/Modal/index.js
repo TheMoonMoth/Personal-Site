3f3a9e1c38975223ed6dd5480b8b285748af63af
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-03-14 13:25:13 -0600

    desktop style for were

--- src/Projects/Project3/Modal/index.js
+++ src/Projects/Project3/Modal/index.js
@@ -6,28 +6,40 @@ const AbsModal = props => {
     <div id="absInfo">
       <div className="screens">
         <img src="./images/abs1.png" alt="Absense main screen" />
-          <div>
-            <h4>ABOUT</h4>
-            <p>
-              AbSense was made to simplify the absence reporting process.
-              Rather than having parents call your school and hoping that the voice
-              message makes its way to the right paper copy file,
-              AbSense digitally archives all submitted reports and make them easily
-              accessible to administrators and families.
-            </p>
-            <p>&nbsp;</p>
-            <h5>BUILT WITH:</h5>
-            <ul>
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>Express</li>
-              <li>Node.js</li>
-            </ul>
-            <aside id="links">
-              <a target="_blank" rel="noopener noreferrer" href="https://absense-671c0.firebaseapp.com/">AbSense.com</a>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/TheMoonMoth/AbSense">GITHUB</a>
-            </aside>
-          </div>
+        <div>
+          <h4>ABOUT</h4>
+          <p>
+            AbSense was made to simplify the absence reporting process. Rather
+            than having parents call your school and hoping that the voice
+            message makes its way to the right paper copy file, AbSense
+            digitally archives all submitted reports and make them easily
+            accessible to administrators and families.
+          </p>
+          <p>&nbsp;</p>
+          <h5>BUILT WITH:</h5>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Express</li>
+            <li>Node.js</li>
+          </ul>
+          <aside id="links">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://absense-671c0.firebaseapp.com/"
+            >
+              AbSense.com
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/TheMoonMoth/AbSense"
+            >
+              GITHUB
+            </a>
+          </aside>
+        </div>
       </div>
     </div>
   )
